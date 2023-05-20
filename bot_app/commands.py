4f1b2797f207115eaaa84e38bac6a4554d9d7d57
Timur Glazunov <timurglazunov@mail.ru>
2023-05-21 02:37:51 +0300

    Переписал логику формирования сообщений

--- bot_app/commands.py
+++ bot_app/commands.py
@@ -3,18 +3,18 @@
 """
 
 from aiogram import types
-
-from bot_app.settings import connection
 from bot_app import messages
 
 from bot_app import dp
-from bot_app.helpers import *
+from bot_app.utils import *
 
 import bot_app.data_fetcher as data_fetcher
 
 from bot_app.settings import Student as StudentData
 
-StudentData  # Здесь должна проходить авторизация пользователя, и инициализация дата класса студента
+# Здесь должна проходить авторизация пользователя, и инициализация дата класса студента
+
+StudentData.group_id = data_fetcher.get_group_id('ППСА-3') 
 
 
 # Первый запуск
@@ -34,33 +34,22 @@ async def buttons(message: types.Message):
     ]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=buttons, one_time_keyboard=True)
-    await message.answer(text="Расписание на:", reply_markup=keyboard)
+    await message.answer(text=messages.START_SCHEDULE, reply_markup=keyboard)
 
 
 # Получить расписание по ID даты в базе расписаний
-async def getLessonsByDate(message: types.Message, date_id):
-    lessons = ''
-
+async def getLessonsByDateID(message: types.Message, date_id: int):
     # Если расписание найдено, то выводим занятие с соответствующей датой
     # Иначе пишем, что занятий в расписании нет
     if (date_id):
-        res = await data_fetcher.get_today_lessons(StudentData.group_id, date_id=date_id)
-        for i in range(len(res)):
-            teachers = ''
-            teacher_list = res[i].get("teacher")
-
-            for j in range(len(teacher_list)):
-                teachers += f'{teacher_list[j].get("name")},'
-            teachers = teachers[:-1]
-
-            lessons += messages.LESSON.substitute(
-                lesson_type=res[i].get("lesson_type"),
-                auditorium=res[i].get("auditorium"),
-                teacher=teachers,
-                lesson_time=res[i].get("lesson_time")
-            )
-
-        return await message.reply(lessons)
+        res = await data_fetcher.get_dated_lessons(StudentData.group_id, date_id)
+        
+        # Если в попытках получить запрос что-то сломалось, 
+        # то сообщим об этом пользоваетлю
+        if not res:
+            await message.reply(messages.SOMETHING_BROKEN)
+        else:
+            return await message.reply(fillLessonsMessage(res=res))
     else:
         await message.reply(messages.NO_LESSONS)
 
@@ -69,50 +58,37 @@ async def getLessonsByDate(message: types.Message, date_id):
 @dp.message_handler(regexp=f"{messages.TODAY}")
 async def getTodayLessons(messages: types.Message):
     date_id = data_fetcher.get_date_id(getTodayDate())
-    await getLessonsByDate(messages, date_id=date_id)
+    await getLessonsByDateID(messages, date_id=date_id)
 
 
 # Получить занатия на следующий день от текущего
 @dp.message_handler(regexp=f"{messages.TOMOROW}")
 async def getTomorrowLessons(messages: types.Message):
     date_id = data_fetcher.get_date_id(getTomorrowDate())
-    await getLessonsByDate(messages, date_id=date_id)
+    await getLessonsByDateID(messages, date_id=date_id)
 
 
-# Получение расписания на эту неделю
+async def getLessonsByWeekNumber(message: types.Message, week_number:int):
+    res = await data_fetcher.get_week_lessons(StudentData.group_id, week_number)
+    return await message.reply(fillLessonsMessage(res=res))
+
+
+# Получение расписания на первую неделю
 @dp.message_handler(regexp=f"{messages.THIS_WEEK}")
 async def getThisWeekLessons(message: types.Message):
-    try:
-        cursor = connection.cursor()
-        # todo добавить запрос на получение данных
-        cursor.execute("")
-        dbData = cursor.fetchall()
-
-        if (dbData != []):
-            return await message.answer(dbData)
-
-        return await message.answer(messages.NO_THIS_WEEK)
-
-    except:
-        await message.answer(messages.SOMETHING_BROKEN)
+    await getLessonsByWeekNumber(message=message, week_number=getWeekNumber())
 
 
-# Получение расписания на следующую неделю
+# Получение расписания на вторую неделю
 @dp.message_handler(regexp=f"{messages.NEXT_WEEK}")
 async def getNextWeekLessons(message: types.Message):
-    try:
-        cursor = connection.cursor()
-
-        # todo добавить запрос на получение данных
-        cursor.execute("")
-        dbData = cursor.fetchall()
-
-        if (dbData != []):
-            return await message.answer(dbData)
-
-        return await message.answer(messages.NO_NEXT_WEEK)
-    except:
-        await message.answer(messages.SOMETHING_BROKEN)
+    week_number = getWeekNumber()
+    
+    # Если текущая неделя первая, то показывает вторую. Иначе наоборот
+    if week_number == 1:
+        await getLessonsByWeekNumber(message=message, week_number=2)
+    else:
+        await getLessonsByWeekNumber(message=message, week_number=1)
 
 
 # Любой текст, который не попал в остальные команды
@@ -131,16 +107,3 @@ async def getNextWeekLessons(message: types.Message):
 #         await message.answer(userName)
 #     except:
 #         await message.answer(f"Введите целое число от 1 до {len(dbData)}")
-
-
-# TODO:
-# 1. Команда по получению занятий сегодня. Сверяешь текущий день (date.today) c датами в бд, и распознаешь день недели, (date.weekday).
-# Если текущей даты нет, то выводишь что-то типа 'сегодня нет занятий. Радостно'.
-# В ином случае показываешь (пока-что) все занятия, назначенные на эту дату)
-# 2. Команда по получению занятий на завтра (аналогично верхнему, но +1 день с проверкой на выходной)
-# 3. Команда по получению всех занятий на неделю.
-# Вот тут пока смутно понимаю, как это сделать. В голову приходит идея ориентироваться на данные о четности недели.
-# То есть, по ближайшим датам понять, какая сейчас неделя (первая, или вторая),
-# и показать по запросу студента все занятия данной недели.
-# 4. Аналогично верхнему, но после определения текущей недели нужно выбрать нужную (если первая, то вторая, иначе первая),
-# и вывести все занятия уже для нее
