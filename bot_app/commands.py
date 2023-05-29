9e531fe6c3042284b0fce2baffb28eb6a0b59227
Timur Glazunov <timurglazunov@mail.ru>
2023-05-30 00:10:16 +0300

    Изменил дизайн кнопок, подредактировал шаблон сообщений

--- bot_app/commands.py
+++ bot_app/commands.py
@@ -14,26 +14,29 @@ from bot_app.settings import Student as StudentData
 
 # Здесь должна проходить авторизация пользователя, и инициализация дата класса студента
 
-StudentData.group_id = data_fetcher.getGroupID('ППСА-3') 
+StudentData.group_id = data_fetcher.getGroupID('ППСА-3')
 
 
 # Первый запуск
 @dp.message_handler(commands=['start'])
 async def startDialog(message: types.Message):
-    await message.reply(messages.GREETINGS)
+    await message.reply(messages.GREETINGS.substitute(user=message.from_user.username))
 
 
 # Показ кнопок для получения расписания
 @dp.message_handler(commands=['schedule'])
 async def showButtons(message: types.Message):
-    buttons = [
-        [types.KeyboardButton(text=f"{messages.TODAY}")],
-        [types.KeyboardButton(text=f"{messages.TOMOROW}")],
-        [types.KeyboardButton(text=f"{messages.THIS_WEEK}")],
-        [types.KeyboardButton(text=f"{messages.NEXT_WEEK}")]
-    ]
+    
+    today_button = types.KeyboardButton(text="Занятия сегодня")
+    tommorow_button = types.KeyboardButton(text="Занятия завтра")
+    this_week_button = types.KeyboardButton(text="Текущая неделя")
+    next_week_button = types.KeyboardButton(text="Следующая неделя")
+    
+    
     keyboard = types.ReplyKeyboardMarkup(
-        keyboard=buttons, one_time_keyboard=True)
+        one_time_keyboard=False, resize_keyboard=True
+    ).row(today_button, tommorow_button).row(this_week_button, next_week_button)
+
     await message.answer(text=messages.START_SCHEDULE, reply_markup=keyboard)
 
 
@@ -43,13 +46,13 @@ async def sendLessonsByDateID(message: types.Message, date_id: int):
     # Иначе пишем, что занятий в расписании нет
     if (date_id):
         res = await data_fetcher.getDatedLessons(StudentData.group_id, date_id)
-        
-        # Если в попытках получить запрос что-то сломалось, 
+
+        # Если в попытках получить запрос что-то сломалось,
         # то сообщим об этом пользоваетлю
         if not res:
             await message.reply(messages.SOMETHING_BROKEN)
         else:
-           return await message.reply(utils.fillLessonsMessage(res=res))
+            return await message.reply(utils.fillLessonsMessage(res=res))
     else:
         await message.reply(messages.NO_LESSONS)
 
@@ -62,16 +65,16 @@ async def sendTodayLessons(messages: types.Message):
 
 
 # Отправить занятия на следующий день от текущего
-@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.TOMOROW))
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.TOMORROW))
 async def sendTomorrowLessons(messages: types.Message):
     date_id = data_fetcher.getDateID(utils.returnTomorrowDate())
     await sendLessonsByDateID(messages, date_id=date_id)
 
 
 # ===== ОТПРАВИТЬ ЗАНЯТИЯ ПО НОМЕРУ НЕДЕЛИ =====
-async def sendLessonsByWeekNumber(message: types.Message, week_number:int):
+async def sendLessonsByWeekNumber(message: types.Message, week_number: int):
     res = await data_fetcher.getWeekLessons(StudentData.group_id, week_number)
-    return await message.reply(utils.fillLessonsMessage(res=res))
+    return await message.reply(f'Занятия {week_number}-ой недели\n' + utils.fillLessonsMessage(res=res))
 
 
 # Отправить расписание на текущую неделю
@@ -84,7 +87,7 @@ async def sendThisWeekLessons(message: types.Message):
 @dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.NEXT_WEEK))
 async def sendNextWeekLessons(message: types.Message):
     week_number = utils.returnWeekNumber()
-    
+
     # Если текущая неделя первая, то показывает вторую. Иначе наоборот
     if week_number == 1:
         return await sendLessonsByWeekNumber(message=message, week_number=2)
