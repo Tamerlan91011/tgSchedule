a76e5ac63e19d2a93e99df1887aa55f19affb50e
Timur Glazunov <timurglazunov@mail.ru>
2023-05-29 22:51:00 +0300

    Изменил принцип ответов. Теперь бот отвечает по наличию ключевых слов

--- bot_app/commands.py
+++ bot_app/commands.py
@@ -6,7 +6,7 @@ from aiogram import types
 from bot_app import messages
 
 from bot_app import dp
-from bot_app.utils import *
+from bot_app import utils
 
 import bot_app.data_fetcher as data_fetcher
 
@@ -14,18 +14,18 @@ from bot_app.settings import Student as StudentData
 
 # Здесь должна проходить авторизация пользователя, и инициализация дата класса студента
 
-StudentData.group_id = data_fetcher.get_group_id('ППСА-3') 
+StudentData.group_id = data_fetcher.getGroupID('ППСА-3') 
 
 
 # Первый запуск
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
+async def startDialog(message: types.Message):
     await message.reply(messages.GREETINGS)
 
 
 # Показ кнопок для получения расписания
 @dp.message_handler(commands=['schedule'])
-async def buttons(message: types.Message):
+async def showButtons(message: types.Message):
     buttons = [
         [types.KeyboardButton(text=f"{messages.TODAY}")],
         [types.KeyboardButton(text=f"{messages.TOMOROW}")],
@@ -37,74 +37,62 @@ async def buttons(message: types.Message):
     await message.answer(text=messages.START_SCHEDULE, reply_markup=keyboard)
 
 
-# ===== ПОЛУЧИТЬ РАСПИСАНИЕ ПО ID ДАТЫ  =====
-async def getLessonsByDateID(message: types.Message, date_id: int):
+# ===== ОТПРАВИТЬ РАСПИСАНИЕ ПО ID ДАТЫ  =====
+async def sendLessonsByDateID(message: types.Message, date_id: int):
     # Если расписание найдено, то выводим занятие с соответствующей датой
     # Иначе пишем, что занятий в расписании нет
     if (date_id):
-        res = await data_fetcher.get_dated_lessons(StudentData.group_id, date_id)
+        res = await data_fetcher.getDatedLessons(StudentData.group_id, date_id)
         
         # Если в попытках получить запрос что-то сломалось, 
         # то сообщим об этом пользоваетлю
         if not res:
             await message.reply(messages.SOMETHING_BROKEN)
         else:
-            return await message.reply(fillLessonsMessage(res=res))
+           return await message.reply(utils.fillLessonsMessage(res=res))
     else:
         await message.reply(messages.NO_LESSONS)
 
 
-# Получить занятия на текущий день
-@dp.message_handler(regexp=f"{messages.TODAY}")
-async def getTodayLessons(messages: types.Message):
-    date_id = data_fetcher.get_date_id(getTodayDate())
-    await getLessonsByDateID(messages, date_id=date_id)
+# Отправить занятия на текущий день
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.TODAY))
+async def sendTodayLessons(messages: types.Message):
+    date_id = data_fetcher.getDateID(utils.returnTodayDate())
+    await sendLessonsByDateID(messages, date_id=date_id)
 
 
-# Получить занятия на следующий день от текущего
-@dp.message_handler(regexp=f"{messages.TOMOROW}")
-async def getTomorrowLessons(messages: types.Message):
-    date_id = data_fetcher.get_date_id(getTomorrowDate())
-    await getLessonsByDateID(messages, date_id=date_id)
+# Отправить занятия на следующий день от текущего
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.TOMOROW))
+async def sendTomorrowLessons(messages: types.Message):
+    date_id = data_fetcher.getDateID(utils.returnTomorrowDate())
+    await sendLessonsByDateID(messages, date_id=date_id)
 
 
-# ===== ПОЛУЧИТЬ ЗАНЯТИЯ ПО НОМЕРУ НЕДЕЛИ =====
-async def getLessonsByWeekNumber(message: types.Message, week_number:int):
-    res = await data_fetcher.get_week_lessons(StudentData.group_id, week_number)
-    return await message.reply(fillLessonsMessage(res=res))
+# ===== ОТПРАВИТЬ ЗАНЯТИЯ ПО НОМЕРУ НЕДЕЛИ =====
+async def sendLessonsByWeekNumber(message: types.Message, week_number:int):
+    res = await data_fetcher.getWeekLessons(StudentData.group_id, week_number)
+    return await message.reply(utils.fillLessonsMessage(res=res))
 
 
-# Получение расписания на текущую неделю
-@dp.message_handler(regexp=f"{messages.THIS_WEEK}")
-async def getThisWeekLessons(message: types.Message):
-    await getLessonsByWeekNumber(message=message, week_number=getWeekNumber())
+# Отправить расписание на текущую неделю
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.THIS_WEEK))
+async def sendThisWeekLessons(message: types.Message):
+    return await sendLessonsByWeekNumber(message=message, week_number=utils.returnWeekNumber())
 
 
-# Получение расписания на следующую неделю
-@dp.message_handler(regexp=f"{messages.NEXT_WEEK}")
-async def getNextWeekLessons(message: types.Message):
-    week_number = getWeekNumber()
+# Отправить расписания на следующую неделю
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in messages.NEXT_WEEK))
+async def sendNextWeekLessons(message: types.Message):
+    week_number = utils.returnWeekNumber()
     
     # Если текущая неделя первая, то показывает вторую. Иначе наоборот
     if week_number == 1:
-        await getLessonsByWeekNumber(message=message, week_number=2)
+        return await sendLessonsByWeekNumber(message=message, week_number=2)
     else:
-        await getLessonsByWeekNumber(message=message, week_number=1)
+        return await sendLessonsByWeekNumber(message=message, week_number=1)
 
 
 # Любой текст, который не попал в остальные команды
-# @dp.message_handler()
-# async def allMessagesHandler(message: types.Message):
-#     cursor = connection.cursor()
-#     cursor.execute(allPersonnelUser)
-
-#     dbData = cursor.fetchall()
-
-#     try:
-#         intUserWithId = dbData[int(message.text) - 1]
-#         userName = intUserWithId[1]
-
-#         await message.answer(intUserWithId)
-#         await message.answer(userName)
-#     except:
-#         await message.answer(f"Введите целое число от 1 до {len(dbData)}")
+@dp.message_handler()
+async def allMessagesHandler(message: types.Message):
+    await message.reply(messages.SOMETHING_BROKEN)
